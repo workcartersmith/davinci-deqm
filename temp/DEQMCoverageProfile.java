ca02e6b9b11d89d4d152c4e124628b3cd3882e4f
Healthedata1 <ehaas@healthedatainc.com>
2020-06-09 11:02:02 -0700

    testing my base tempalte

--- temp/DEQMCoverageProfile.java
+++
@@ -1,7 +0,0 @@
-package http://hl7.org/fhir/us/davinci-deqm/ImplementationGuide/hl7.fhir.us.davinci-deqm-1.1.0;
-
-import org.hl7.fhir.r5.model.ProfilingWrapper;
-
-public class DEQMCoverageProfile {
-
-}
